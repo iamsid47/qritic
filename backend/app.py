9b13a4dea76721deeb746f81c10969c102c21b4d
Siddhesh Kulthe <siddheshkulthe43@gmail.com>
2023-09-28 22:38:22 +0530

    add password hashing

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Body
 from pymongo import MongoClient
 from argon2 import PasswordHasher
 
@@ -12,8 +12,12 @@ class User:
     def __init__(self, username, email, password, company):
         self.username = username
         self.email = email
-        self.password = PasswordHasher(password)
+        self.password_hash = self.hash_password(password)
         self.company = company
+    
+    def hash_password(self, password):
+        ph = PasswordHasher()
+        return ph.hash(password)
 
 
 @app.get("/")
@@ -26,11 +30,13 @@ def create_user_account(user: User):
     user_data = {
         "username" : user.username,
         "email": user.email,
-        "password": user.password,
+        "password": user.password_hash,
         "company": user.company
     }
 
     result = collection.insert_one(user_data)
 
     if result.inserted_id:
-        return {"message": ""}
+        return {"message": "User created successfully", "userID": result.inserted_id}
+    else:
+        raise HTTPException(status_code=500, detail="Failed to create user")
