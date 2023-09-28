758c2ae298d3d804ead309e46502a46d03d60848
Siddhesh Kulthe <siddheshkulthe43@gmail.com>
2023-09-28 23:20:41 +0530

    add login

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI, HTTPException, Body
+from fastapi import FastAPI, HTTPException, Body, Depends
 from pymongo import MongoClient
-from argon2 import PasswordHasher
+from argon2 import PasswordHasher, exceptions
+from pydantic import BaseModel
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from decouple import config
+
+
+SECRET_KEY = config("SECRET_KEY")
+ALGORITHM = config("ALGORITHM")
 
 app = FastAPI()
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["qritic"]
-collection = db["users"]
+client = MongoClient(config("DATABASE_URL"))
+db = client[config("DATABASE_NAME")]
+collection = db[config("DATABASE_COLLECITON")]
+
+class UserInCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+    company: str
 
 class User:
     def __init__(self, username, email, password, company):
@@ -22,21 +36,65 @@ class User:
 
 @app.get("/")
 def read_root():
-    return "Hello World"
+    return "Server is running"
 
 
 @app.post("/create_account")
-def create_user_account(user: User):
+def create_user_account(user: UserInCreate):
     user_data = {
         "username" : user.username,
         "email": user.email,
-        "password": user.password_hash,
+        "password": User(user.username, user.email, user.password, user.company).password_hash,
         "company": user.company
     }
 
     result = collection.insert_one(user_data)
 
     if result.inserted_id:
-        return {"message": "User created successfully", "userID": result.inserted_id}
+        return {"message": "User created successfully", "userID": str(result.inserted_id)}
     else:
         raise HTTPException(status_code=500, detail="Failed to create user")
+    
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+@app.post("/login")
+def login(user_data: UserLogin):
+    user = collection.find_one({"email": user_data.email})
+    
+    if user is None:
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    ph = PasswordHasher()
+    
+    try:
+        if not ph.verify(user_data.password, user["password"]):
+            raise HTTPException(status_code=400, detail="Invalid credentials")
+    except exceptions.InvalidHash:
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    access_token = create_access_token(data={"sub": user["email"]})
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+
+from fastapi.security import OAuth2PasswordBearer
+from typing import Optional
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+@app.get("/protected-data")
+def protected_data(current_user: str = Depends(oauth2_scheme)):
+    return {"message": "This is protected data.", "user": current_user}
