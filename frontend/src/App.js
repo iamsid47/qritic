f0fe57b33c75a0ee451ad7bae09a17574f72e19c
Siddhesh Kulthe <siddheshkulthe43@gmail.com>
2023-09-29 15:23:24 +0530

    add login ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Pages/Home";
+import Login from "./components/Auth/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
