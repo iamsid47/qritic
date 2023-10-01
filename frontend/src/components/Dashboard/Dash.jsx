1f1667f807f637fd40c8d24b260bf651e4cb4496
Siddhesh Kulthe <siddheshkulthe43@gmail.com>
2023-10-01 23:26:36 +0530

    create account page

--- frontend/src/components/Dashboard/Dash.jsx
+++ frontend/src/components/Dashboard/Dash.jsx
@@ -1,50 +1,11 @@
 import React from "react";
-import { useAuth } from "../Auth/AuthContext";
-import { useNavigate } from "react-router-dom";
 
 import Layout from "./Layout";
 
 const Dash = () => {
-  const { user, logout } = useAuth();
-
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    logout();
-    navigate("/");
-  };
   return (
     <Layout>
-      {user ? (
-        <div className="">
-          <div className="">
-            <br />
-
-            <>
-              <p>{user.userEmail}</p>
-              <p>{user.userCompany}</p>
-              <p>{user.userName}</p>
-              <p>{user.jobTitle}</p>
-              <button
-                className="px-4 py-2 bg-blue-600 rounded-xl text-white"
-                onClick={handleLogout}
-              >
-                Logout
-              </button>
-            </>
-          </div>
-        </div>
-      ) : (
-        <div className="min-h-screen flex flex-col text-white bg-sky-700 items-center mx-auto justify-center space-y-8">
-          <p className="text-center text-2xl">
-            You are not logged in
-            <span className="text-4xl font-medium px-3">:&#40;</span>
-          </p>
-          <a href="/login" className="secondarybtn">
-            Login &rarr;
-          </a>
-        </div>
-      )}
+      <h1 className="text-4xl">Dashboard</h1>
     </Layout>
   );
 };
